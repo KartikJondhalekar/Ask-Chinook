51738f0c5e7c850ad2edd2d76aa18c95e6e0a4ae
jondhalekark <jondhalekar.k@northeastern.edu>
2025-12-18 17:59:26 -0500

    Initial setup and basic UI design

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import styles from './index.module.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [queryDescription, setQueryDescription] = useState('');
+  const [sqlQuery, setSqlQuery] = useState('');
+
+  const onFormSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(queryDescription);
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className="d-flex align-items-center mb-4 gap-2">
+        <i className="bi bi-database-fill h1 m-0"></i>
+        <h1 className='m-0'>Ask Chinook</h1>
+      </div>
+
+      <form onSubmit={onFormSubmit}>
+        <input type='text'
+          name='query-description'
+          placeholder='Ask me anything about the Chinook database...'
+          onChange={(e) => { setQueryDescription(e.target.value) }} />
+        <input type='submit' value='Ask' />
+      </form>
+    </main>
+  )
+}
+
+export default App
